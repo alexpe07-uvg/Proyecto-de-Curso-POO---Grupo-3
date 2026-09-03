b996df353533d4fdd494146120aa6af2686c3d3f
Otto Andrés De León Lemus <133066224+andre-link@users.noreply.github.com>
2026-09-03 07:46:59 -0600

    editado, comentario de test
    Added a comment for testing purposes.

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
+  //andrelink, prueba de testeo
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UVG Matches',
